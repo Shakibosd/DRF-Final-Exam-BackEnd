24eff40d6e85f2f7d57c2ffb3d4a74d38ccf01f2
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-10-19 00:33:58 +0600

    first commit

--- flower_seal/settings.py
+++ flower_seal/settings.py
@@ -14,13 +14,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-LOGIN_URL = 'http://127.0.0.1:5500/login.html'
+LOGIN_URL = "http://127.0.0.1:5500/login.html"
 
 # Application definition
 INSTALLED_APPS = [
