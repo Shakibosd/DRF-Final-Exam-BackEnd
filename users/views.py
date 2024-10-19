67c97b7d9a104b6a9a83395a6c8521f2e4427fc0
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-10-19 13:58:05 +0600

    finish

--- users/views.py
+++ users/views.py
@@ -51,9 +51,9 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
+        return redirect('https://flower-seal.netlify.app/login.html')
     else:
-        return redirect('register') 
+        return redirect('https://flower-seal.netlify.app/register.html') 
 
 #user login korar jonno
 class LoginAPIView(APIView):
