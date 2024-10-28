d7eca4dcb507b33df5a2d494c3e16353dc849f76
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-10-28 19:57:22 +0600

    payment added

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,7 @@
 from sslcommerz_lib import SSLCOMMERZ 
 import random, string
 from django.http import HttpResponse, HttpResponseRedirect
-from flowers.models import Flower
+# from flowers.models import Flower
 
 def unique_transaction_id_generator(size=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -12,26 +12,25 @@ def payment(request):
     sslcz = SSLCOMMERZ(settings)
     
     post_body = {}
-    post_body['total_amount'] = Flower.price #এখানে ওরা ডামি একটা পেমেন্ট দিছে, কিন্তু আমার প্রোডাক্ট এর প্রাইস যত টাকা টিক তত টাকা পেমেন্ট দিবে হবে ইউজার কে।
+    post_body['total_amount'] = 100.12 #এখানে ওরা ডামি একটা পেমেন্ট দিছে, কিন্তু আমার প্রোডাক্ট এর প্রাইস যত টাকা টিক তত টাকা পেমেন্ট দিবে হবে ইউজার কে।
     post_body['currency'] = "BDT"
     post_body['tran_id'] = unique_transaction_id_generator(),
     post_body['success_url'] = "https://flower-seal-backend.vercel.app/orders/my_orders/"
     post_body['fail_url'] = "https://flower-seal-backend.vercel.app/orders/my_orders/"
     post_body['cancel_url'] = "https://flower-seal-backend.vercel.app/flowers/flowers/"
     post_body['emi_option'] = 0
-    post_body['cus_name'] = request.user.username #যে ইউজার প্রোডাক্ট টি কিনবে তার নাম এখানে সো হবে। 
-    post_body['cus_email'] = request.user.email #যে ইউজার পোডাক্ট টি কিনবে তার ইমেইল এখানে সো হবে। 
-    post_body['cus_phone'] = "01700000000"#যে ইউজার পোডাক্ট টি কিনবে তার মোবাইল এখানে সো হবে। 
-    post_body['cus_add1'] = "Cantonment"#যে ইউজার পোডাক্ট টি কিনবে তার ঠিকানা এখানে সো হবে।
-    post_body['cus_city'] = "Dhaka"#যে ইউজার পোডাক্ট টি কিনবে তার সিটি এখানে সো হবে।
-    post_body['cus_country'] = "Bangladesh"#যে ইউজার পোডাক্ট টি কিনবে তার দেশ এখানে সো হবে।
+    post_body['cus_name'] =  request.user.username
+    post_body['cus_email'] = request.user.email
+    post_body['cus_phone'] = "01700000000" #যে ইউজার পোডাক্ট টি কিনবে তার মোবাইল এখানে সো হবে। 
+    post_body['cus_add1'] = "Cantonment" #যে ইউজার পোডাক্ট টি কিনবে তার ঠিকানা এখানে সো হবে।
+    post_body['cus_city'] = "Dhaka" #যে ইউজার পোডাক্ট টি কিনবে তার সিটি এখানে সো হবে।
+    post_body['cus_country'] = "Bangladesh" #যে ইউজার পোডাক্ট টি কিনবে তার দেশ এখানে সো হবে।
     post_body['shipping_method'] = "NO"
     post_body['multi_card_name'] = ""
     post_body['num_of_item'] = 1
-    post_body['product_name'] = Flower.title
-    post_body['product_category'] = Flower.category
+    post_body['product_name'] = "Flower title"
+    post_body['product_category'] = "Flower category"
     post_body['product_profile'] = "general"
-
 
     response = sslcz.createSession(post_body) # API response
     # print(response)
