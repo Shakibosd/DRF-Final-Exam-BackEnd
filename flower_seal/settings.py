e64a00b1bf8997a5863c1163b8cbc6547f380764
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2025-01-12 03:28:50 +0600

    version 1 er kaj finish alhamdulillah 1

--- flower_seal/settings.py
+++ flower_seal/settings.py
@@ -64,8 +64,8 @@ AUTHENTICATION_BACKENDS = [
 # AUTH_USER_MODEL = 'profiles.CustomUser'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:5500",
+    "https://flower-seal-backend.vercel.app",
+    "https://flower-seal.netlify.app",
 ]
 
 MIDDLEWARE = [
