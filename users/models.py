391a09a96d3a23357d08e48aec768106ca1e007e
shakibosd <syednazmusshakib94@gmail.com>
2025-03-02 00:59:15 +0600

    feat(user): user model max_langth change

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    profile_img = models.CharField(max_length=200, default='')
+    profile_img = models.CharField(max_length=1000, default='')
     otp = models.CharField(max_length=6, blank=True, null=True)
 
     def __str__(self):  
